6e71c08d35e95e8b891740706d09e05e4016adfe
Olivia Hornback <olivia.hornback@gmail.com>
2020-04-08 18:04:59 -0400

    Start to put the random in random event

--- Game Files/src/RandomEvent/RandomEvent.gd
+++ Game Files/src/RandomEvent/RandomEvent.gd
@@ -1,7 +1,22 @@
 extends VBoxContainer
 
+onready var prompt = get_node("ColorRect/Label2")
+onready var button_one = get_node("ColorRect/Button")
+onready var button_two = get_node("ColorRect/Button2")
+onready var be_good_prompts = {0: ["Prompt", "Choice 1", "Choice 2"], 1 : ["a", "b", "c"], 2: ["Prompt 3", "Choice 1", "Choice 2"]}
 
 func _ready():
+	var new_prompt = "You hand a customer a cup of lemonade. As you do,  a bug flies into the cup. The customer has not noticed."
+	button_one.set_text("Say Nothing.")
+	button_two.set_text("Pour a new cup")
+	set_prompt(new_prompt)
+	display_prompt()
+	pass # Replace with function body.
+
+
+func _on_RandomEventContainer_visibility_changed():
+	# In the future, have a list of random events that occur
+	# stuff them in array and pop one out at the end of the day
 	pass # Replace with function body.
 
 
@@ -10,8 +25,20 @@ func _on_Button_pressed():
 	self.visible = false
 	
 
-
-func _on_RandomEventContainer_visibility_changed():
-	# In the future, have a list of random events that occur
-	# stuff them in array and pop one out at the end of the day
+func _on_Button2_pressed():
 	pass # Replace with function body.
+
+func set_prompt(string) -> void:
+	prompt.text = string
+
+func display_prompt() -> void:
+	# grab a random prompt and display it
+	var rng = RandomNumberGenerator.new()
+	var rand = rng.randi_range(0,be_good_prompts.size()-1)
+	print(rand)
+	print(rand)
+	print(rand)
+	#print(be_good_prompts[rand])
+	#set_prompt(be_good_prompts[rand][0])
+	#button_one.set_text(be_good_prompts[rand][1])
+	#button_two.set_text(be_good_prompts[rand][2])
